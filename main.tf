403e61d8c745805c37fc02c92fae819f2d69f679
Yeshwanth351 <75407470+Yeshwanth351@users.noreply.github.com>
2022-09-14 18:34:42 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -4,13 +4,6 @@ access_key  = "AKIAWULY2HIYCWVHQRMT"
 secret_key = "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"
 }
 
-
-
-resource "aws_instance" "terraform_created_instance" {
-ami = "ami-0960ab670c8bb45f3"
-instance_type = "t2.micro"
-key_name = "terraform"
-tags = {
-Name = "terraform_created_instance"
-}
+module "create-ec2" {
+  source = "/home/ubuntu/terraform/module/"
 }
