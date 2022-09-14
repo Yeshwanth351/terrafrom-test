47e3deb0c4efcf233168237d87d22c22ba9ae7e2
Yeshwanth351 <75407470+Yeshwanth351@users.noreply.github.com>
2022-09-14 17:59:34 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -7,9 +7,9 @@ secret_key = "lvUjTcQ3h7L1r+GSebMoLsOGFx8Q364t84gcYidG"
 
 
 resource "aws_instance" "terraform_created_instance" {
-ami = "ami-0729e439b6769d6ab"
+ami = "ami-0960ab670c8bb45f3"
 instance_type = "t2.micro"
-key_name = "travis"
+key_name = "terraform"
 tags = {
 Name = "terraform_created_instance"
 }
