97453875dfdcc1a7cd81f7f2ab06c34a5d95c428
Yeshwanth351 <75407470+Yeshwanth351@users.noreply.github.com>
2022-09-14 19:06:23 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -4,7 +4,7 @@ access_key  = "AKIAWULY2HIYCWVHQRMT"
 secret_key = "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"
 }
 
-module "terraform-aws-module" {
-  source  = "spacelift.io/yeshwanth351/terraform-aws-module/aws"
+module "terraform-aws-module-2" {
+  source  = "spacelift.io/yeshwanth351/terraform-aws-module-2/aws"
   version = "0.1.0"
 }
